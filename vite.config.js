ae8abb82b4e03e2fda9118be23cb461072f43095
Layomi <layomilawore@gmail.com>
2025-09-04 10:06:41 +0100

    fixing path issues

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { fileURLToPath } from "url";
 
+// Recreate __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  base: import.meta.env.VITE_BASE_PATH || "/new-portfolio.git",
+    base: env.VITE_BASE_PATH || "/new-portfolio.git",
+  };
 });
